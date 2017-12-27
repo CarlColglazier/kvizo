33e45681927057b4299ec3ee12914bfad883f6d7
Carl Colglazier <carl@carlcolglazier.com>
2017-12-27 15:09:39 -0500

    Add allowed hosts for production.

--- backend/kvizo/settings.py
+++ backend/kvizo/settings.py
@@ -16,7 +16,9 @@ from .secret import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kvizo.carlcolglazier.com',
+]
 
 
 # Application definition
